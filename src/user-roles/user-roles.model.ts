1bce35ce3ac9b1913e3ea396dea8af47a206a522
Stas Kozak <kozak.stasan@gmail.com>
2022-11-23 13:38:42 +0200

    feature: implement module, service, controller to roles table, implement adding default role when user created

---
+++ src/user-roles/user-roles.model.ts
@@ -0,0 +1,32 @@
+import {
+  Column,
+  DataType,
+  ForeignKey,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+import { User } from '../users/users.model';
+import { Role } from '../roles/roles.model';
+
+@Table({ tableName: 'user_roles', createdAt: false, updatedAt: false })
+export class UserRoles extends Model<UserRoles> {
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @ForeignKey(() => Role)
+  @Column({
+    type: DataType.INTEGER,
+  })
+  roleId: string;
+
+  @ForeignKey(() => User)
+  @Column({
+    type: DataType.INTEGER,
+  })
+  userId: string;
+}
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,5 +1,13 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
 import { ApiProperty } from '@nestjs/swagger';
+import { Role } from '../roles/roles.model';
+import { UserRoles } from '../user-roles/user-roles.model';
 
 interface UserCreationAttrs {
   email: string;
@@ -60,4 +68,7 @@ export class User extends Model<User, UserCreationAttrs> {
     allowNull: true,
   })
   banReason: string;
+
+  @BelongsToMany(() => Role, () => UserRoles)
+  roles: Role[];
 }
